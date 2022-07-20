4c0bafc4cf9303b283b543d3ecdd3d6c62b53e88
PrusovAB <djromeo93@mail.ru>
2022-07-20 21:23:53 +0300

    addes new web site

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+const sidebar = document.querySelector('.sidebar');
+sidebar.addEventListener('mouseover', (event) => {
+	sidebar.style.left = "0px"
+})
+sidebar.addEventListener('mouseout', () => {
+	sidebar.style.left = "-302px"
+})
+
+
+
+
